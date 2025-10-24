9075198103c5f29bfa64f4085fa905f7e2cef3cf
Взрослая академия <reshetov@mail.ru>
2025-10-24 19:35:35 +0300

    f1: генерация и вывод поля, config

--- main.py
+++ main.py
@@ -1,2 +1,38 @@
+from config import lvl_size
+field = []
+
+
+def show_field():
+    for i in field:
+        for j in i:
+            print(j, end=" ")
+        print()
+
+
+def generate_field():
+    global field
+
+    lvl = choose_lvl()
+    size=lvl_size[lvl]
+    field = [[0 for j in range(size)] for i in range(size)]
+    # for i in range(size):
+    #     field.append([])
+    #     for j in range(size):
+    #         field[i].append(0)
+
+
+def choose_lvl():
+    try:
+        lvl = int(input("выберите уровень (1/2/3): "))
+        if lvl not in [1, 2, 3]:
+            raise Exception
+    except:
+        print("некорректные данные")
+        choose_lvl()
+
+    return lvl
+
+
 if __name__ == '__main__':
-    print("игра сапер")
+    generate_field()
+    show_field()
